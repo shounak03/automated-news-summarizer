aa9ed3577ddb8d8710c8917d9319c9455356ae3b
shounak03 <shounakmis.3@gmail.com>
2024-11-11 00:19:54 +0530

    added about component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Summary from './components/Summary'
 import Header from './components/header'
 import Footer from './components/Footer'
 import NewsDisplay from './components/NewsPage'
+import About from './components/About'
 
 function App() {
 
@@ -14,10 +15,10 @@ function App() {
     <Routes>
       <Route path="/" element={<NewsDisplay/>} />
       <Route path="/:title" element={<Summary/>} />
+      <Route path="/about" element={<About/>} />
     </Routes>
     <Footer/>
     </>
-
   )
 }
 
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-gray-900 p-4 text-center text-gray-600 border-t border-gray-600">
+    <footer className="bg-gray-900 p-4 text-center text-white border-t border-gray-600">
         <p>&copy; 2023 News Summary App. All rights reserved.</p>
       </footer>
   )
