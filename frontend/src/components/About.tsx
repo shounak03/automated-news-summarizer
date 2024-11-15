1459090b76705eceef9441fd724c7be152fb0a57
shounak03 <shounakmis.3@gmail.com>
2024-11-15 23:36:15 +0530

    about section updated

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,7 +1,19 @@
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen bg-gray-900 flex flex-col">
+      <main className="flex-grow p-4 flex flex-col m-4">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold mb-4 text-white">About Our News Summarizer</h2>
+        <p className="text-lg mb-6 text-white">
+          This app provides a convenient way to get summaries of the latest news articles. Simply browse the headlines on the home page, click on any article to view the full content, then click the "Summary" button to generate a concise summary.
+        </p>
+        <p className="text-lg text-white">
+          Our goal is to help you stay informed without getting bogged down in lengthy articles. We leverage advanced language models to analyze article content and deliver insightful summaries tailored to your needs.
+        </p>
+      </div>
+    </main>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom"
+
 function Footer() {
   return (
-    <footer className="bg-gray-900 p-4 text-center text-white border-t border-gray-600">
-        <p>&copy; 2023 News Summary App. All rights reserved.</p>
-      </footer>
+   
+      <footer className="border-t p-4 border-gray-600 bg-gray-900">
+      <div className="container mx-auto flex justify-between items-center">
+     <p className="text-xs text-gray-400">© 2023 News Summary App. All rights reserved.</p>
+     <div className="flex space-x-4">
+       <Link
+         className="text-xs  text-gray-400 hover:text-purple-400 transition-colors"
+         to="/terms-of-service"
+         aria-label="Terms of Service"
+       >
+         Terms of Service
+       </Link>
+       <Link
+         className="text-xs text-gray-400 hover:text-purple-400 transition-colors"
+         to="/privacy-policy"
+         aria-label="Privacy Policy"
+       >
+         Privacy
+       </Link>
+     </div>
+   </div>
+ </footer>
   )
 }
 
